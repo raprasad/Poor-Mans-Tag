6acbe5d909e0ca265c23da4a102ed21852fb33f6
Raman Prasad <raman_prasad@harvard.edu>
2014-03-20 15:57:53 -0400

    updated comment

---
+++ README.md
@@ -0,0 +1,15 @@
+Used in other Django projects to "tag" content.
+
+For example both a NewsStory and FacultyMember may have an M2M relationship to Tag
+    class NewsStory(models.Model):
+        ...
+        tags = models.ManytoManyField(Tag)
+
+    class FacultyMember(models.Model):
+        ...
+        tags = models.ManytoManyField(Tag)
+        
+In this example, the sharing of tags allows a NewsStory to be loosely linked to a FacultyMember as well as other models, etc.  Or the tags only used within a single model may be used for classification.
+
+(Why a separate "repository"?  Don't ask.) 
+
--- tags/models.py
+++ tags/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 class Tag(models.Model):
+    """Used in other Django projects to "tag" content.
+    
+    For example both a NewsStory and FacultyMember may have an M2M relationship to Tag
+        class NewsStory(models.Model):
+            ...
+            tags = models.ManytoManyField(Tag)
+
+        class FacultyMember(models.Model):
+            ...
+            tags = models.ManytoManyField(Tag)
+            
+    In this example, the sharing of tags allows a NewsStory to be loosely linked to a FacultyMember as well as other models, etc.
+
+     """
     name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(max_length=255, blank=True)
     visible = models.BooleanField(default=True)
